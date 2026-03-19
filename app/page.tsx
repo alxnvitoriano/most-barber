f069d77b90062ce10e9fb5a9491aa21d48828444
alxnvitoriano <alxnvtn@gmail.com>
2026-03-19 04:34:02 -0300

    feat: add barbershop items components

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,26 @@ import Header from "./components/header";
 import SearchInputs from "./components/search-inputs";
 import banner from "../public/banner.png";
 import BookingItem from "./components/booking-item";
+import { prisma } from "@/lib/prisma";
+import BarbershopItems from "./components/barbershop-items";
 
-const Home = () => {
+const Home = async () => {
+  const recommendedBarbeshops = await prisma.barbershop.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  const popularBarbershops = await prisma.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  });
+
   return (
     <div>
       <Header />
-      <div className="space-y-4 px-5">
+      <div className="space-y-4 p-5">
         <SearchInputs />
         <Image
           src={banner}
@@ -22,10 +36,26 @@ const Home = () => {
         </h2>
         <BookingItem
           serviceName="Corte de cabelo"
-          barbershopName="Barbearia do Alan"
+          barbershopName="Barbearia do Chef"
           barbershopImageUrl="https://utfs.io/f/5c89f046-80cd-4443-89df-211de62b7c2a-17p.png"
-          date={new Date()}
+          date={new Date().toISOString()}
         />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {recommendedBarbeshops.map((barbershop) => (
+            <BarbershopItems key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItems key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   );
